d025893ea309934e10ad2c287f1ebb550643dc2a
FEUILLATRE Nicolas <nicolas.feui@gmail.com>
2026-07-01 18:33:21 +0300

    add: Implement getAllPossibleMoves with getPawnMoves and getRookMoves stubs

--- ChessEngine.py
+++ ChessEngine.py
@@ -46,12 +46,33 @@ class Gamestate():
     All moves considering checks
     """
     def getValidMoves(self):
-        pass
+        return self.getAllPossibleMoves() #for now we will not worry about checks
 
     """
     All moves without considering checks
     """
     def getAllPossibleMoves(self):
+        moves = []
+        for r in range(len(self.board)): #number of rows
+            for c in range(len(self.board[r])): #number of cols given row
+                turn = self.board[r][c][0] #Index [0] grabs the first character (the color). So turn is either 'w', 'b', or '-'
+                if (turn == 'w' and self.whiteToMove) or (turn == 'b' and not self.whiteToMove):
+                    piece = self.board[r][c][1] #Index [1] grabs the type of the piece
+                    if piece == 'p':
+                        self.getPawnMoves(r,c , moves)
+                    elif piece == 'R':
+                        self.getRookMoves(r, c, moves)
+
+    """
+    Get all the pawn moves for the pawns, and add those moves to the list
+    """
+    def getPawnMoves(self, r, c, moves):
+        pass
+
+    """
+    Get all the rook moves for the pawns, and add those moves to the list
+    """
+    def getRookMoves(self, r, c, moves):
         pass
 
 
