e6084e0c6e92c729077ddfed8b743b980b9a0db1
FEUILLATRE Nicolas <nicolas.feui@gmail.com>
2026-07-01 16:24:44 +0300

    add: implement undoMove in Gamestate

--- ChessEngine.py
+++ ChessEngine.py
@@ -23,11 +23,24 @@ class Gamestate():
         self.whiteToMove = True
         self.moveLog = []
 
+    """
+    Takes a Move as a parameter and executes it (don't work for castling, pawn promotion and en passant)
+    """
     def makeMove(self, move):
         self.board[move.startRow][move.startCol] = "--"
         self.board[move.endRow][move.endCol] = move.pieceMoved
         self.moveLog.append(move) #log the move so we can undo it later
         self.whiteToMove = not self.whiteToMove #swap players
+
+    """
+    Undo the last move made
+    """
+    def undoMove(self):
+        if len(self.moveLog) != 0: #make sure that there is a move to undo
+            move = self.moveLog.pop() #pop() removes and returns the last item in the list
+            self.board[move.startRow][move.startCol] = move.pieceMoved
+            self.board[move.endRow][move.endCol] = move.pieceCaptured
+            self.whiteToMove = not self.whiteToMove #switch turns back
 
 
 class Move():
