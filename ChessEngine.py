2cff5470342a65df0097a71e907726ce0b487380
FEUILLATRE Nicolas <nicolas.feui@gmail.com>
2026-07-01 19:34:38 +0300

    add: implement moveID and override equality method for Move class

--- ChessEngine.py
+++ ChessEngine.py
@@ -99,7 +99,17 @@ class Move():
         self.endCol = endSq[1]
         self.pieceMoved = board[self.startRow][self.startCol]
         self.pieceCaptured = board[self.endRow][self.endCol]
+        self.moveID = self.startRow * 1000 + self.startCol * 100 + self.endRow * 10 + self.endCol
 
+    """
+    Overriding the equals method
+    """
+    def __eq__(self, other):
+        if isinstance(other, Move):
+            return self.moveID == other.moveID
+        return False
+    
+    
     def getRankFile(self, r, c):
         return self.colsToFiles[c] + self.rowsToRanks[r]
 
